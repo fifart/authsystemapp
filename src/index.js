e09eb6d3fbc322c1b6848bd586cc8117a08a0daa
ANASTASIOS PANAGOPOULOS <tasssop-macbook-pro@DESKTOP-N6H23BN.station>
2023-03-05 11:14:18 +0200

    repo init on authsystemapp

---
+++ src/index.js
@@ -0,0 +1,34 @@
+require('./models/User')
+const express = require('express')
+const mongoose = require('mongoose')
+const authRoutes = require('./routes/authRoutes')
+const requireAuth = require('./middlewares/requireAuth')
+
+
+const app = express()
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(authRoutes)
+
+const mongoUri = 'mongodb+srv://admin:passwordx2@cluster0.yebrx.mongodb.net/authapp?retryWrites=true&w=majority'
+mongoose.connect(mongoUri)
+mongoose.connection.on('connected', ()=>{
+    console.log('Mongo DB connected');
+})
+mongoose.connection.on('error', (err)=>{
+    console.log('Erron connection with error', err );
+})
+
+
+
+app.get('/', requireAuth, (req,res)=>{
+    res.status(200).send('Welcome');
+})
+
+app.listen(3001, ()=>{
+    console.log('Server is listening on port 3001');
+})
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const User = mongoose.model('User')
+
+const router = express.Router()
+
+
+router.post('/signup',async (req,res)=>{
+    const { email, password } = req.body
+    try {
+        const user = new User({email, password})
+        await user.save()
+        const token = jwt.sign({userId: user._id },'MyAwsomeSecretKey')
+        res.send({token})
+    } catch (err) {
+        res.status(422).send(err.message)
+    }
+})
+
+
+router.post('/signin', async (req,res)=> {
+    const { email, password } = req.body
+
+    if (!email || !password ) {
+        return res.status(422).send({error: "Must provide email and password"})
+    }
+
+    const user = await User.findOne( { email })
+
+    if(!user) {
+        return res.status(422).send({error: 'Email not found'})
+    }
+
+    try {
+        await user.comparePassword(password)
+        const token = jwt.sign({userId: user.id}, 'MyAwsomeSecretKey')
+        res.send({token})
+    } catch (error) {
+        res.status(422).send({error: 'Sonething was wrong with given credentials'})
+    }
+
+   
+})
+
+
+
+
+
+
+module.exports = router
